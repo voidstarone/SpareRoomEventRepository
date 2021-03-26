e795762ba9ea7afcdbad597bc5026fd76645bcfb
Thomas Lee <thomas.lee999@gmail.com>
2021-03-26 23:00:01 +0000

    Add support filtering for events before a date (#11)

--- Sources/SpeedRoommatingEventRepository/Interfaces/Repo/ISpeedRoommatingEventRepo.swift
+++ Sources/SpeedRoommatingEventRepository/Interfaces/Repo/ISpeedRoommatingEventRepo.swift
@@ -8,6 +8,7 @@ public protocol ISpeedRoommatingEventRepo {
     func listAllEvents(onComplete: @escaping (Result<[ISpeedRoommatingDTOEvent], Error>) -> Void)
     func listAllEventsOrderedByStartTimeAscending(onComplete: @escaping (Result<[ISpeedRoommatingDTOEvent], Error>) -> Void)
     func listAllEventsOnOrAfter(date: Date, onComplete: @escaping (Result<[ISpeedRoommatingDTOEvent], Error>) -> Void)
+    func listAllEventsBefore(date: Date, onComplete: @escaping (Result<[ISpeedRoommatingDTOEvent], Error>) -> Void)
     func listAllEventsByYear(onComplete: @escaping (Result<[Int:[ISpeedRoommatingDTOEvent]], Error>) -> Void)
     func listAllEventsByYearThenMonth(onComplete: @escaping (Result<[Int:[Int:[ISpeedRoommatingDTOEvent]]], Error>) -> Void)
 }
